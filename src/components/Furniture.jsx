d0eff9b1992498424279934bbdd7f256555cb913
Quinn Shannon <quinn.p.shannon@gmail.com>
2026-04-20 17:52:39 -0400

    Adding some Components

---
+++ src/components/Furniture.jsx
@@ -0,0 +1,24 @@
+export function Bed(){
+    
+}
+export function Trunk(){
+
+}
+export function Dresser(){
+
+}
+export function Endtable(){
+
+}
+export function TVStand(){
+
+}
+export function Couch(){
+
+}
+export function Chair(){
+
+}
+export function Desk(){
+
+}
